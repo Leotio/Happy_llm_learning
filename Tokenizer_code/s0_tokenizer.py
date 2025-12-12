647c93de45ae719fba479538c0892ad5d1ea824d
Leotio <3107362992@qq.com>
2025-12-12 23:12:42 +0800

    commit message

--- Tokenizer_code/s0_tokenizer.py
+++ Tokenizer_code/s0_tokenizer.py
@@ -1,1 +1,22 @@
 '''使用Hugging Face的Tokenizers库来训练一个BPE Tokenizer'''
+import random
+import json
+import os
+from transformers import AutoTokenizer, PreTrainedTokenizerFast
+from tokenizers import (
+decoders,
+models,
+pre_tokenizers,
+trainers,
+Tokenizer,
+)
+from tokenizers.normalizers import NFKC
+from typing import Generator
+
+'''加载训练数据'''
+
+'''创建配置文件'''
+
+'''训练BPE Tokenizer'''
+
+'''使用训练好的Tokenizer'''
